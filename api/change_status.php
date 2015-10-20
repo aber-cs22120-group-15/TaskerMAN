1cc31a4332044a1958b446f9cf25a929f3a2f182
GLaDOSDan <gladosdan@gmail.com>
2015-10-20 17:53:05 +0100

    - Added api/change_status

---
+++ api/change_status.php
@@ -0,0 +1,43 @@
+<?php
+
+$task_id = (int) $core->IO->get('id');
+$status = (int) $core->IO->get('status');
+
+$task = new task($task_id);
+
+if (empty($task->id)){
+	// Unable to load task, throw error
+	echo $API->error('Invalid task ID');
+	exit;
+}
+
+switch ($status){
+
+	// Make sure we're not setting the status to a state it's already in
+	case $task->status:
+		echo $API->error('Trying to change status to same value as it already has');
+		exit;
+	break;
+
+	// allocated
+	case 1:
+		$task->setStatus(1);
+		$task->setCompletedTime();
+	break;
+
+	// completed
+	case 2:
+		$task->setStatus(2);
+		$task->setCompletedTime('0000-00-00 00:00:00');
+	break;
+
+	// Invalid status code
+	default:
+		echo $API->error('Invalid status value (must be 1 or 2)');
+		exit;
+	break;
+}
+
+$task->save();
+
+var_dump($task);
