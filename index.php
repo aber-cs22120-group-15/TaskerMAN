947b472f4c0249bdbe21e9b695a33930b8972fc7
GLaDOSDan <gladosdan@gmail.com>
2015-10-25 18:56:35 +0000

    - Fixed ?p= variable not allowing underscores

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once('config/init.php');
 
 // Determine which page the user wants to see
 if (isset($_GET['p']) && !empty($_GET['p'])){
-	$core->Page->setPage(preg_replace("/[^A-Za-z0-9 ]/", '', $core->IO->get('p')));
+	$core->Page->setPage(preg_replace("/[^A-Za-z0-9_ ]/", '', $core->IO->get('p')));
 
 	if (!file_exists('pages/' . $core->Page->page . '.php')){
 		$core->Page->page = '404';
