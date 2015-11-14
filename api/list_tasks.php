0306d027b3d8e9cae238a9bfad396927cbe8080f
GLaDOSDan <gladosdan@gmail.com>
2015-11-14 15:08:42 +0000

    - Limited api/list_tasks to returning just 25 tasks for testing purposes

--- api/list_tasks.php
+++ api/list_tasks.php
@@ -1,7 +1,8 @@
 <?php
 
 TaskerMAN\TaskListInterface::setSearchCriteria('assignee_uid', TaskerMAN\API::$uid);
-
+TaskerMAN\TaskListInterface::setStartPosition(0);
+TaskerMAN\TaskListInterface::setLimit(25);
 
 echo TaskerMAN\API::response(array('tasks' => TaskerMAN\TaskListInterface::getTasks()));
 exit;
