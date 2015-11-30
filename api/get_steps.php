3d3c0136ff2e3909615fd1e9889324fec2b2873a
GLaDOSDan <gladosdan@gmail.com>
2015-11-30 15:05:13 +0000

    - Reworked get_steps to support multiple tasks in one request

--- api/get_steps.php
+++ api/get_steps.php
@@ -1,17 +1,29 @@
 <?php
 
-$id = (int) IO::GET('id');
+$ids = explode(',', IO::GET('id'));
 
-$task = new TaskerMAN\Task($id);
-
-if (is_null($task->id)){
-	// Unable to load task
-	throw new TaskerMAN\APIErrorException('Task does not exist');
+foreach ($ids as $key => $id){
+	if (!is_numeric($id)){
+		unset($ids[$key]);
+	}
 }
 
-if ((int) $task->assignee_uid !== TaskerMAN\API::$uid){
-	throw new TaskerMAN\APIErrorException('User does not have access to this task');
+$steps = array();
+
+foreach ($ids as $id){
+
+	$task = new TaskerMAN\Task($id);
+	if (is_null($task->id)){
+		// Unable to load task
+		throw new TaskerMAN\APIErrorException('Task ' . $id . ' does not exist');
+	}
+
+	if ((int) $task->assignee_uid !== TaskerMAN\API::$uid){
+		throw new TaskerMAN\APIErrorException('User does not have access to task ' . $id);
+	}
+
+	$steps[$id] = $task->getSteps();
+
 }
 
-
-echo TaskerMAN\API::response(array('steps' => $task->getSteps()));
+echo TaskerMAN\API::response(array('steps' => $steps));
