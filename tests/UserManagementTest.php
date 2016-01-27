f3b65f9e67ac3d80ce3eb8e6d9b6e54e23892c22
GLaDOSDan <gladosdan@gmail.com>
2016-01-27 22:54:42 +0000

    - Documented UserManagementTests

--- tests/UserManagementTest.php
+++ tests/UserManagementTest.php
@@ -2,17 +2,21 @@
 
 class UserManagementTest extends PHPUnit_Framework_TestCase {
 	
+	/**
+	 * Test that the system allows valid passwords
+	 */
 	public function testValidPasswords(){
 
 		$passwords = array('hello!', 'thiswill!work', 'but#will#this');
-
 		foreach ($passwords as $password){
 
 			$this->assertTrue(TaskerMAN\Application\UserManagement::validatePassword($password));
 		}
-	
 	}
 
+	/**
+	 * Test that the system rejects invalid passwords
+	 */
 	public function testInvalidPasswords(){
 
 		$this->setExpectedException('TaskerMAN\Application\UserManagementException');
@@ -22,7 +26,6 @@ class UserManagementTest extends PHPUnit_Framework_TestCase {
 
 			$this->assertFalse(TaskerMAN\Application\UserManagement::validatePassword($password));
 		}
-	
 	}
 
 
